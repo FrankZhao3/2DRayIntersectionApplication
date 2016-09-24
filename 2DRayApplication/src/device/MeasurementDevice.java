4a6152cea250077e8c2cfed105f22064f6f83560
FrankZhao3 <hpzhao3@berkeley.edu>
2016-09-23 23:08:26 -0700

    setting up

--- 2DRayApplication/src/device/MeasurementDevice.java
+++ 2DRayApplication/src/device/MeasurementDevice.java
@@ -1,5 +1,39 @@
 package device;
 
 public class MeasurementDevice {
+	Position position; 
+	Double offsetAngle;
+	Double facingAngle;	// angle offset between x-axis (counter-clockwise)
+	
+	public MeasurementDevice(Double positionX, Double positionY, Double offsetAngle, Double facingAngle) {
+		position = new Position(positionX, positionY);
+		this.offsetAngle = offsetAngle;
+		this.facingAngle = facingAngle;
+	}
+	
+	public Double getPositionX() {
+		return position.getX();
+	}
+	public void setPositionX(Double positionX) {
+		position.setX(positionX);
+	}
+	public Double getPositionY() {
+		return position.getY();
+	}
+	public void setPositionY(Double positionY) {
+		position.setY(positionY);
+	}
+	public Double getOffsetAngle() {
+		return offsetAngle;
+	}
+	public void setOffsetAngle(Double offsetAngle) {
+		this.offsetAngle = offsetAngle;
+	}
+	public Double getFacingAngle() {
+		return facingAngle;
+	}
+	public void setFacingAngle(Double facingAngle) {
+		this.facingAngle = facingAngle;
+	}
 
 }
---
+++ 2DRayApplication/src/device/Position.java
@@ -0,0 +1,26 @@
+package device;
+
+public class Position {
+	private double x = 0;
+	private double y = 0;
+	
+	//default constructor
+	public Position() {}
+	// constructor takes in x, y positions
+	public Position(double x, double y) {
+		this.setX(x);
+		this.setY(y);
+	}
+	public double getX() {
+		return x;
+	}
+	public void setX(double x) {
+		this.x = x;
+	}
+	public double getY() {
+		return y;
+	}
+	public void setY(double y) {
+		this.y = y;
+	}
+}
